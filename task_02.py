61f84e8b559772c69fbcc74e48ceba7ad07d8ede
ghutake <aniketghutake888@gmail.com>
2025-07-25 15:47:22 +0530

    Add files via upload

---
+++ task_02.py
@@ -0,0 +1,10 @@
+import math
+num = float(input("Enter a number: "))
+
+sqrt_value = math.sqrt(num)
+log_value = math.log(num)
+sine_value = math.sin(num)
+
+print(f"\nSquare root of {num} is: {sqrt_value}")
+print(f"Natural logarithm of {num} is: {log_value}")
+print(f"Sine of a {num} is: {sine_value}")
